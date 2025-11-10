c4e1b92e21ed2cb0f5beb1efee920904de7990e7
Isaac Thor <37605399+codethor0@users.noreply.github.com>
2025-11-10 14:50:27 -0600

    test(android): add baseline unit and instrumentation coverage

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -63,7 +63,12 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test:core-ktx:1.6.1")
+    androidTestImplementation("androidx.test:rules:1.6.1")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
 }
 
